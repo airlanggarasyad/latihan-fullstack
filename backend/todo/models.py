b48d8f810fcffe0938c82be34c91ec77d6d7682d
airlanggafidiyanto <airlangga.rasyad@live.com>
2021-10-08 14:12:03 +0700

    api + frontend

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    isCompleted = models.BooleanField(default=False)
+
+    def _str(self):
+        return self.title
